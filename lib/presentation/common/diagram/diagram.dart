93e84dd707f5fd092f423578a4f65f95df64e07a
BekoevPavel <pablank@bk.ru>
2022-06-02 10:06:25 +0300

    добавил diagram в common

--- lib/presentation/common/diagram/diagram.dart
+++ lib/presentation/common/diagram/diagram.dart
@@ -5,7 +5,7 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:get/get.dart';
-import '../../domain/entities/entities.dart';
+import '../../../domain/entities/entities.dart';
 
 part 'widget/diagram_widget.dart';
 part 'states/diagram_state.dart';
