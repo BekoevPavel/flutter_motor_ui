0105e2e7f1919812146d7ffbd9149b19ecd3f0bc
BekoevPavel <pablank@bk.ru>
2022-06-12 01:29:38 +0300

    -

--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -3,7 +3,7 @@ import 'core/core.dart';
 import 'domain/usecases/common/common.dart';
 
 void initDI() {
-  //Get.put<ConnectToArduino>(ConnectToArduino(), permanent: true);
+  Get.put<ConnectToArduino>(ConnectToArduino(), permanent: true);
   Get.put<AllData>(AllData());
   Get.put<SpeedometrController>(SpeedometrController());
   Get.put<Sizes>(Sizes());
